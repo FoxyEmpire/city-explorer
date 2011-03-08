df2815ed0f039ab3ed01522cbfa1d49b5d26022b
marcel@famlehmann.ch <marcel@famlehmann.ch@ab964af1-2025-2167-89e9-b6705e674a88>
2011-03-08 14:56:13 +0000

    Erste Version von HomeActivity. Ist nun Start Activity und ruft StartActivity auf beim 1. Button.

---
+++ trunk/src/CityExplorer/src/ch/bfh/CityExplorer/Activities/HomeActivity.java
@@ -0,0 +1,29 @@
+package ch.bfh.CityExplorer.Activities;
+
+import ch.bfh.CityExplorer.R;
+import ch.bfh.CityExplorer.StartActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View.OnClickListener;
+import android.view.View;
+
+public class HomeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+        findViewById(R.id.btnMap).setOnClickListener(new OnClickListener(){
+        	public void onClick(View v){
+        		OnClickMap(v);
+        	}
+    	});
+    }
+    
+    protected void OnClickMap(View view){
+    	Intent intent = new Intent(this,StartActivity.class);
+		startActivity(intent);
+    }
+}
