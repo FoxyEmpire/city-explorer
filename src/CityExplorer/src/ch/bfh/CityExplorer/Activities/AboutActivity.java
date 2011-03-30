f1b03859382e14ae89df5ec2a26121169532aaeb
marcel@famlehmann.ch <marcel@famlehmann.ch@ab964af1-2025-2167-89e9-b6705e674a88>
2011-03-30 15:48:24 +0000

    icons augestauscht About hinzugefügt

---
+++ src/CityExplorer/src/ch/bfh/CityExplorer/Activities/AboutActivity.java
@@ -0,0 +1,13 @@
+package ch.bfh.CityExplorer.Activities;
+
+import ch.bfh.CityExplorer.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+	}
+}
