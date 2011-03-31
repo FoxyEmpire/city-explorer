098421746b5d55b3f10932f0a14ca84c1709ddbb
pascal.irminger@gmail.com <pascal.irminger@gmail.com@ab964af1-2025-2167-89e9-b6705e674a88>
2011-03-31 16:18:58 +0000

    CHN: Calendar permissions entfernt und Duplikate von Favs verhindert.

--- trunk/src/CityExplorer/src/ch/bfh/CityExplorer/Data/CityExplorerStorage.java
+++ trunk/src/CityExplorer/src/ch/bfh/CityExplorer/Data/CityExplorerStorage.java
@@ -2,6 +2,7 @@ package ch.bfh.CityExplorer.Data;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
@@ -17,7 +18,23 @@ public class CityExplorerStorage {
 		Log.d(TAG, "Kontaktspeicher angelegt.");
 	}
 
-	public long InsertFavourite(int poiId) {
+	public boolean InsertFavourite(int poiId) {
+		SQLiteDatabase dbConRead = mDb.getReadableDatabase();
+
+		try {
+			Cursor cursor = dbConRead.query(
+					FavouriteTbl.TABLE_NAME,
+					FavouriteTbl.ALL_COLUMNS,
+					FavouriteTbl.POI_ID + " = ?",
+					new String[] { String.valueOf(poiId) },
+					null, null, null);
+			if (cursor.getCount() > 0) {
+				return false;
+			}
+		} catch (Exception ex) {
+			String s = ex.getMessage();
+		}
+
 		final ContentValues daten = new ContentValues();
 		daten.put(FavouriteTbl.POI_ID, poiId);
 
@@ -26,7 +43,7 @@ public class CityExplorerStorage {
 		try {
 			final long id = dbCon.insertOrThrow(FavouriteTbl.TABLE_NAME, null, daten);
 			Log.i(TAG, "Favourite mit id=" + id + " erzeugt.");
-			return id;
+			return true;
 		} finally {
 			dbCon.close();
 		}
