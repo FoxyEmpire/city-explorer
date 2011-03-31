5b7d242e5a661153698403ea7ef1d8ddc9645c14
marcel@famlehmann.ch <marcel@famlehmann.ch@ab964af1-2025-2167-89e9-b6705e674a88>
2011-03-31 13:26:47 +0000

    bei zuvielen anfragen auf google wird nun eine meldung angezeigt

---
+++ src/CityExplorer/src/ch/bfh/CityExplorer/Activities/ImageActivity.java
@@ -0,0 +1,87 @@
+package ch.bfh.CityExplorer.Activities;
+
+import ch.bfh.CityExplorer.R;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.PointF;
+import android.graphics.RectF;
+import android.os.Bundle;
+import android.util.FloatMath;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.ImageView;
+
+public class ImageActivity extends Activity implements OnTouchListener {
+	
+	// These matrices will be used to move and zoom image
+	   Matrix matrix = new Matrix();
+	   Matrix savedMatrix = new Matrix();
+	   
+	   private PointF start = new PointF();
+	   private PointF mid = new PointF();
+	   private float oldDist = 1f;
+
+	   private ImageView imageView;
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.image);
+        
+        Bundle extra = getIntent().getExtras();
+        if (extra != null){
+        	byte[] data = extra.getByteArray("image");
+        	Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+        	imageView = (ImageView)findViewById(R.id.imageView);
+        	imageView.setImageBitmap(bitmap);
+        	imageView.setOnTouchListener(this);
+        }
+        else{
+        	finish();
+        }
+        
+        matrix.setTranslate(1f, 1f);
+        imageView.setImageMatrix(matrix);
+	}
+	
+	@Override
+	   public boolean onTouch(View v, MotionEvent event) {
+	      ImageView view = (ImageView) v;
+
+	      // Handle touch events here...
+	      switch (event.getAction() & MotionEvent.ACTION_MASK) {
+	       case MotionEvent.ACTION_POINTER_DOWN:
+	    	   oldDist = spacing(event);
+	    	   savedMatrix.set(matrix);
+	    	   midPoint(mid, event);
+	    	   break;
+	    	case MotionEvent.ACTION_MOVE:
+    	      float newDist = spacing(event);
+    	      if (newDist > 10f) {
+    	         matrix.set(savedMatrix);
+    	         float scale = newDist / oldDist;
+    	         matrix.postScale(scale, scale, mid.x, mid.y);
+    	      }
+    	      break;
+	      }
+
+	      // Perform the transformation
+	      view.setImageMatrix(matrix);
+	      view.invalidate();
+	      return true; // indicate event was handled
+	   }
+	
+	private float spacing(MotionEvent event) {
+		   float x = event.getX(0) - event.getX(1);
+		   float y = event.getY(0) - event.getY(1);
+		   return FloatMath.sqrt(x * x + y * y);
+		}
+	private void midPoint(PointF point, MotionEvent event) {
+		   float x = event.getX(0) + event.getX(1);
+		   float y = event.getY(0) + event.getY(1);
+		   point.set(x / 2, y / 2);
+		}
+}
--- src/CityExplorer/src/ch/bfh/CityExplorer/Activities/ImageDisplayer.java
+++ src/CityExplorer/src/ch/bfh/CityExplorer/Activities/ImageDisplayer.java
@@ -1,9 +1,16 @@
 package ch.bfh.CityExplorer.Activities;
 
+import java.io.ByteArrayOutputStream;
+
 import android.R;
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.Bitmap.CompressFormat;
+import android.view.Display;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
@@ -12,11 +19,13 @@ public class ImageDisplayer implements Runnable {
 	public ImageView view;
 	public Bitmap bmp;
 	public ProgressBar pbar;
+	private Display display;
 	
-	public ImageDisplayer(ImageView imageView, Bitmap bmp, ProgressBar pbar) {
+	public ImageDisplayer(ImageView imageView, Bitmap bmp, ProgressBar pbar, Display display) {
 		this.view = imageView;
 		this.bmp = bmp;
 		this.pbar = pbar;
+		this.display = display;
 	}
 	
 	public void run() {
@@ -30,9 +39,34 @@ public class ImageDisplayer implements Runnable {
 		   dialog.show();
 		}
 		else{
-			view.setImageBitmap(bmp);
-			view.setVisibility(View.VISIBLE);
-			pbar.setVisibility(View.GONE);
+			
+			if (bmp.getWidth() > display.getWidth()) {
+				int newWidth = display.getWidth();
+				int newHeight = bmp.getHeight() * newWidth / bmp.getWidth();
+				
+		        float scaleWidth = ((float) newWidth) / bmp.getWidth();
+		        float scaleHeight = ((float) newHeight) / bmp.getHeight();
+				
+		        Matrix matrix = new Matrix();
+		        matrix.postScale(scaleWidth, scaleHeight);
+
+		        Bitmap bmp1 = Bitmap.createBitmap(bmp, 0, 0,
+		        		bmp.getWidth(), bmp.getHeight(), matrix, true);
+		        view.setImageBitmap(bmp1);
+				view.setVisibility(View.VISIBLE);
+				pbar.setVisibility(View.GONE);
+			}
+			
+			view.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View arg0) {
+					Intent intent = new Intent(view.getContext(), ImageActivity.class);
+					ByteArrayOutputStream bos = new ByteArrayOutputStream();     
+					bmp.compress(CompressFormat.PNG,100,bos);  
+					intent.putExtra("image", bos.toByteArray());
+					view.getContext().startActivity(intent);
+				}
+			});
 		}
 	}
  
